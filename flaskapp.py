971ccb4a2f9da45b894e6281eb2669e1d8177c3f
amit <thiszamit@gmail.com>
2023-03-10 09:51:01 +0530

    adding about page in flask

--- flaskapp.py
+++ flaskapp.py
@@ -2,5 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/check')
 def hello():
     return "<h1> Welcome bro <h1>"
+
+@app.route('/about')
+def about():
+    return "You've entered the about page"
+
+if __name__=='__main__':
+    app.run(debug=True)
