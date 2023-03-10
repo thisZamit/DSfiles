21c6f5bd72f162515fd795a7bb34c7e4738f6d1c
amit <thiszamit@gmail.com>
2023-03-10 11:02:28 +0530

    basic flask app using html and render template

--- flaskapp.py
+++ flaskapp.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/check')
 def hello():
-    return "<h1> Welcome bro <h1>"
-
+    return "hello"
 @app.route('/about')
 def about():
-    return "You've entered the about page"
+    return render_template('home.html')
 
 if __name__=='__main__':
     app.run(debug=True)
