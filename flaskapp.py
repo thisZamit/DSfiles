1ec251807aaa9ba1a016ef37ee7e274edc38a5a9
amit <thiszamit@gmail.com>
2023-03-14 01:12:35 +0530

    redirecting to other urls from current url using redirect & url_for

--- flaskapp.py
+++ flaskapp.py
@@ -1,13 +1,29 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for
+
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/check')
-def hello():
-    return "hello"
-@app.route('/about')
-def about():
-    return render_template('home.html')
+# nothing in home page
+
+@app.route('/submit/<int:marks>')       # this will allow you to enter a number in this url and get output accordingly
+def hello(marks):
+    if marks>50:
+        return redirect(url_for('success',score=marks)) # redirect to pass page, observe that function name is passed not url(pass)
+    else:
+        return redirect(url_for('fail',score=marks))    # this will redirect to fail page
+    
+
+@app.route('/pass/<int:score>')
+def success(score):
+    return f"According to score you entered:{score} you have passed"
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return f"According to score you entered:{score} you have failed"
+
+
+
+
+    
 
 if __name__=='__main__':
     app.run(debug=True)
