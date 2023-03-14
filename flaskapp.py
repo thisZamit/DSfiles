e51bbaa2d1ad06f065157f8797d7117566025940
amit <thiszamit@gmail.com>
2023-03-14 08:18:24 +0530

    creating a basic form using <form> in html and request from flask

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,14 @@
-from flask import Flask,render_template,redirect,url_for
+from flask import Flask,render_template,redirect,url_for,request
 
 app = Flask(__name__)
 
-# nothing in home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 
 @app.route('/submit/<int:marks>')       # this will allow you to enter a number in this url and get output accordingly
-def hello(marks):
+def submit(marks):
     if marks>50:
         return redirect(url_for('success',score=marks)) # redirect to pass page, observe that function name is passed not url(pass)
     else:
@@ -14,14 +17,20 @@ def hello(marks):
 
 @app.route('/pass/<int:score>')
 def success(score):
-    return f"According to score you entered:{score} you have passed"
+    return f"According to your score:{score} you have passed"
 
 @app.route('/fail/<int:score>')
 def fail(score):
-    return f"According to score you entered:{score} you have failed"
+    return f"According to your score:{score} you have failed"
 
-
-
+@app.route('/check',methods=['POST','GET'])
+def check():
+    if request.method=='POST':
+        maths = float(request.form['maths'])        # maths is given inside 'name' tag in html file 
+        science = float(request.form['science'])
+        total_score = maths+science
+    
+    return redirect(url_for('submit',marks=total_score))
 
     
 
