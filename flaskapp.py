6ca5ac9bce9bca7553e995aaecad95529271875d
amit <thiszamit@gmail.com>
2023-03-14 09:33:07 +0530

    jinja tools writing code, using inplace variable and commenting in html file{% %}

--- flaskapp.py
+++ flaskapp.py
@@ -9,19 +9,14 @@ def home():
 
 @app.route('/submit/<int:marks>')       # this will allow you to enter a number in this url and get output accordingly
 def submit(marks):
-    if marks>50:
-        return redirect(url_for('success',score=marks)) # redirect to pass page, observe that function name is passed not url(pass)
-    else:
-        return redirect(url_for('fail',score=marks))    # this will redirect to fail page
-    
+    return render_template('output.html',variable=marks)
 
-@app.route('/pass/<int:score>')
-def success(score):
-    return f"According to your score:{score} you have passed"
 
-@app.route('/fail/<int:score>')
-def fail(score):
-    return f"According to your score:{score} you have failed"
+@app.route('/checkfor')
+def checkfor():
+    diction={'name':'Amit','Age':26}
+    return render_template('checkfor.html',variable=diction)
+
 
 @app.route('/check',methods=['POST','GET'])
 def check():
